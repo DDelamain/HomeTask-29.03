e8790c4e4593aaad610d325e84b62511916ca68f
DDelamain <doroshenkodanila1@gmail.com>
2025-04-10 22:32:15 +0500

    Add files via upload

---
+++ Task 3/Task 3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int num, i;
+string sum;
+bool negative = false;
+int main(){
+	setlocale(LC_ALL, "Rus");
+	cout << "Введите целое число: ";
+	cin >> num;
+	if (num < 0) {
+		negative = true;
+		num *= -1;
+	}
+	if (num == 0) {
+		cout << "Ваше число: " << num;
+		return 0;
+	}
+	while (num>0) {
+		if (num % 10 == 3 || num % 10 == 6) {
+			num /= 10;
+			continue;
+		}
+		sum = to_string(num % 10) + sum;
+		num /= 10;
+	}
+	if (negative) {
+		sum = "-" + sum;
+	}
+	cout << "Ваше новое число: " << sum;
+}
