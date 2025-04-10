bfb95642de454a0d5f96b34e6969d374d16d68d2
DDelamain <doroshenkodanila1@gmail.com>
2025-04-10 22:34:45 +0500

    Add files via upload

---
+++ Task 7/Task 7.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int first, second;
+int main(){
+	setlocale(LC_ALL, "Rus");
+	cout << "Введите первое число: ";
+   cin >> first;
+   cout << "Введите второе число: ";
+   cin >> second;
+   if (first < 0) {
+	   first = abs(first);
+   }
+   if (second < 0) {
+	   second = abs(second);
+   }
+   cout << "Ваши числа делятся без остатка на: ";
+   for (int i = 1; i <= min(first, second); i++) {//Я решил попробовать max и min
+	   if (first % i == 0 && second % i == 0) {
+		   cout << i << " ";
+	   }
+   }
+}
